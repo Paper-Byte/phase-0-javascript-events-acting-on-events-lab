ce8ebd53e39c0b29b228151b5e971c298fdb0294
Noah <noahhughesph@gmail.com>
2023-04-19 22:01:08 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+const moveDodgerLeft = () =>{
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const parseLeft = parseInt(leftNumbers);
+
+    if(parseLeft > 0){
+        dodger.style.left = `${parseLeft - 1}px`;
+    }
+}
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const parseLeft = parseInt(leftNumbers);
+
+    if(parseLeft > 0){
+        dodger.style.left = `${parseLeft + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+    if(e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
